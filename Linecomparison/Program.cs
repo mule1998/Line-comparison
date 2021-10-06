21232cfacbc81a46f06d9c4af07a2c1b69e75d1b
Shubham Mule <shubhambmule@gmail.com>
2021-10-06 11:24:18 +0530

    checking equality

--- Linecomparison/Program.cs
+++ Linecomparison/Program.cs
@@ -18,7 +18,18 @@ namespace Linecomparison
             x2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter y2");
             y2 = int.Parse(Console.ReadLine());
-            Console.WriteLine(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+            double length1;
+            double length2;
+            int m1 = 4;
+            int n1 = 3;
+            int m2 = 6;
+            int n2 = 5;
+            length1 = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            length2 = Math.Sqrt((m2 - m1) * (m2 - m1) + (n2 - n1) * (n2 - n1));
+            Console.WriteLine(length1);
+            Console.WriteLine(length2);
+            Console.WriteLine(length1.ToString().Equals(length2.ToString()));
+            //lenght.ToString() is a object to string method and .Equals is used for checking their equality
         }
         
     }
